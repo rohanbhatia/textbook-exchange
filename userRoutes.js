9fe0475195ef52a86e81def944d6a836254883dc
Rohan Bhatia <rohan1413@gmail.com>
2016-11-21 20:24:25 -0500

    work on route files

--- userRoutes.js
+++ userRoutes.js
@@ -1,1 +1,35 @@
 var User = require('../models/user');
+
+// app.post('/signup', users.createUser); 
+// signup
+exports.createUser = function(req, res) {
+
+};
+
+// app.post('/login', users.userLogin);  
+// login
+exports.userLogin = function(req, res) {
+
+};
+
+// app.get('/user', users.getUserInfo); 
+// Get user info / object
+exports.getUserInfo = function(req, res) {
+
+};
+
+// app.post('/editUser', users.editUserInfo);  
+// Post new user edit
+exports.editUserInfo = function(req, res) {
+
+};
+
+// app.post('/removeUser', users.removeUser);  
+// Remove user
+exports.removeUser = function(req, res) {
+
+};
+
+
+
+
