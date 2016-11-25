c958672d1fd412a1349bad1623bc94d557a340ea
Elizabeth Leung <elizabeth.leung@mail.utoronto.ca>
2016-11-25 01:48:32 -0500

    Added form data to JSON helper fn

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,3 +2,21 @@
 function displayError(err){
 	$("#page-content-wrapper").html("<h1>An error has occured. Details: " + err + "</h2>");
 }
+
+/**
+ * Helper function for form buttons to construct a JSON from the fields.
+ *
+ * @param {object} click event
+ * @param {string} formID
+ * @return {object} dataSet
+ *
+ */
+function constructFormJson(evt, formID) {
+  evt.preventDefault();
+  let formData = $(("#"+formID)).serializeArray();
+  let dataSet = {};
+  for (let i = 0; i < formData.length; i++) {
+    dataSet[(formData[i])["name"]] = (formData[i])["value"];
+  }
+  return dataSet;
+}
