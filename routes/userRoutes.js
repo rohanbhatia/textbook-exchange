011ccd37e17a0d35b4de509d0d36e66e53110fe5
Elizabeth Leung <elizabeth.leung@mail.utoronto.ca>
2016-11-29 19:33:05 -0500

    Completed integration on login/signup. Solved adminStatus always returning true, made changes to test_users.json and userRoutes with sol'n.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,41 +1,54 @@
 var Ad = require('../models/ad');
 var User = require('../models/user');
 
-// app.post('/signup', users.createUser); 
+// app.post('/signup', users.createUser);
 // signup
 exports.createUser = function(req, res) {
-	
+
 	console.log("createUser");
 
-	//create new user
-	var newUser = User({
-		email: req.body["email"],
-		password: req.body["password"],
-		first_name: req.body["first_name"],
-		last_name: req.body["last_name"],
-		admin_status: "False"
-	});
+	var user_email = req.body["email"];
 
-	//save new user to db
-	newUser.save(function(err) {
+	User.find({email: user_email}, function(err, user) {
+
 		if (err) throw err;
 
-		console.log("User created");
+		//If user found with the given email, prevent overwrite of the existing data
+		if (user[0]) {
+			return res.send((user_email + " is already in use."));
+		}
+		else {
+			//create new user
+			var newUser = User({
+				email: req.body["email"],
+				password: req.body["password"],
+				first_name: req.body["first_name"],
+				last_name: req.body["last_name"],
+				admin_status: false
+			});
+
+			//save new user to db
+			newUser.save(function(err) {
+				if (err) throw err;
+
+				console.log("User created");
+			});
+			return res.send("Thank you for joining us. Please login to get started!");
+		}
 	});
 
-	res.send("Success");
 };
 
-// app.post('/login', users.userLogin);  
+// app.post('/login', users.userLogin);
 // login
 exports.userLogin = function(req, res) {
-	
+
 	console.log('userLogin');
 
 	var user_email = req.body["email"];
 
 	User.find({email: user_email}, function(err, user) {
-		
+
 		if (err) throw err;
 
 		//user found
@@ -43,19 +56,19 @@ exports.userLogin = function(req, res) {
 
 			var given_password = req.body["password"];
 			var real_password = user[0].password;
-			
+
 			//correct password
 			if (given_password == real_password) {
-				
+
 				//user is not already logged in
 				if (!user[0].logged_in) {
 					//generate token
 					var token = "" + Math.random();
 					user[0].session_token = token;
 					user[0].logged_in = true;
-					// admin_status Always returning true for some reason
-					return res.json({"token": user[0].session_token, "adminStatus": user[0].admin_status,
-                        "email": user[0].email});
+					return res.json({"token": user[0].session_token,
+														"adminStatus": user[0].admin_status,
+                        		"email": user[0].email});
 				}
 				//user already logged in
 				else {
@@ -71,14 +84,14 @@ exports.userLogin = function(req, res) {
 		else {
 			res.send("Failure: No such user\n");
 		}
-		
+
 	});
 };
 
-// app.get('/user', users.getUserInfo); 
+// app.get('/user', users.getUserInfo);
 // Get user info / object
 exports.getUserInfo = function(req, res) {
-	
+
 	console.log('getUserInfo');
 
 	//use req.query
@@ -86,7 +99,7 @@ exports.getUserInfo = function(req, res) {
 	var user_email = req.query.email;
 
 	User.find({email: user_email}, function(err, user) {
-		
+
 		if (err) throw err;
 
 		var result = new Object();
@@ -108,31 +121,31 @@ exports.getUserInfo = function(req, res) {
 		else {
 			res.send("Failure: No such user\n");
 		}
-		
+
 	});
 };
 
-// app.post('/editUser', users.editUserInfo);  
+// app.post('/editUser', users.editUserInfo);
 // Post new user edit
 exports.editUserInfo = function(req, res) {
-	
+
 	console.log('editUserInfo');
-	
+
 	var user_email = req.body["email"];
 
 	User.find({email: user_email}, function(err, user) {
-		
+
 		if (err) throw err;
 
 		console.log(req.body.token);
 		console.log(req.body.email);
-		
+
 		user[0].email = req.body.email;
 		user[0].first_name = req.body.firstName;
 		user[0].last_name = req.body.lastName;
 		user[0].password = req.body.password;
 		user[0].save(function(err)	{
-			
+
 			if (err) throw err;
 
 			res.send("Success\n");
@@ -141,10 +154,10 @@ exports.editUserInfo = function(req, res) {
 	});
 };
 
-// app.delete('/removeUser', users.removeUser);  
+// app.delete('/removeUser', users.removeUser);
 // Remove user
 exports.removeUser = function(req, res) {
-	
+
 	console.log('removeUser');
 
 	var user_email = req.body["email"];
@@ -155,7 +168,7 @@ exports.removeUser = function(req, res) {
 
 		//user found
 		if (user[0]) {
-			
+
 			user[0].remove(function(err) {
 
 				if (err) throw err;
@@ -171,5 +184,3 @@ exports.removeUser = function(req, res) {
 
 	});
 };
-
-
