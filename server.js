674d1a7cd5b00cb956580216e46e11bbdcf222df
Timothy Lock <lock.k.timothy@gmail.com>
2016-12-01 08:47:37 -0500

    Added email support. Dont spam pls

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var users = require('./routes/userRoutes');
 var ads = require('./routes/adRoutes');
 var comments = require('./routes/commentRoutes');
 var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
 
 var app = express();
 app.use(express.static(__dirname + '/assets'));
@@ -17,6 +18,29 @@ app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
     extended: true
 }));
 
+// Set up a mail transporter
+// create reusable transporter object using the default SMTP transport
+// Please DON'T share this with anybody. Even though its just temporary for our project, I don't need google blacklisting me
+var transporter = nodemailer.createTransport('smtps://donotreply_uoftextbook%40timothylock.ca:eDtU;Y;g4}NgBz$M@smtp.zoho.com');
+
+/* Example calling 
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    from: '"Fred Foo ?" <foo@blurdybloop.com>', // sender address
+    to: 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers
+    subject: 'Hello ✔', // Subject line
+    text: 'Hello world ?', // plaintext body
+    html: '<b>Hello world ?</b>' // html body
+};
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+});
+*/
 
 app.post('/signup', users.createUser);  // signup
 app.post('/login', users.userLogin);  // login
